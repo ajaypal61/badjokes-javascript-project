21581899ff4928b7eada966fbfc3de85955f5b1e
ajaypal61 <ajaypal2987@gmail.com>
2022-12-15 19:56:10 -0800

    change

--- script.js
+++ script.js
@@ -4,56 +4,54 @@
 // GET https://icanhazdadjoke.com/ 
 
 
-const jokes = document.querySelector('#joke');
-const jokeBtn = document.querySelector('#jokeBtn');
-
-const generatejokes =()=>{
-
-    const setHeader= {
-        headers: {
-            Accept: "application/json"
-        }
-    }
-
-    fetch('https://icanhazdadjoke.com', setHeader)
-    .then((res)=>res.json())
-    .then((data)=>{
-        jokes.innerHTML=data.joke;
-    }).catch((error)=>{
-        console.log(error);
-    })
-}
-
-
-
-
-// 2. async await 
-
 // const jokes = document.querySelector('#joke');
 // const jokeBtn = document.querySelector('#jokeBtn');
 
-// const generatejokes = async () => {
+// const generatejokes =()=>{
 
-//    try{
 //     const setHeader= {
 //         headers: {
 //             Accept: "application/json"
 //         }
 //     }
 
-//     const res = await fetch('https://icanhazdadjoke.com', setHeader)
-//     const data = await res.json();
-//     jokes.innerHTML=data.joke;
-//    }catch(err){
-//     console.log(`the eror is${err}`);
-//    }
-
-// }
-
-
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         jokes.innerHTML=data.joke;
+//     }).catch((error)=>{
+//         console.log(error);
+//     })
+// };
 
 
+// jokeBtn.addEventListener('click', generatejokes);
+// generatejokes();
 
+
+
+// 2. async await 
+
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+const generatejokes = async () => {
+
+   try{
+    const setHeader= {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', setHeader)
+    const data = await res.json();
+    jokes.innerHTML=data.joke;
+   }catch(err){
+    console.log(`the eror is${err}`);
+   }
+
+}
 
 
 jokeBtn.addEventListener('click', generatejokes);
